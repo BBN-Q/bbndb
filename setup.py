9c733f0bab70267ed8ff0a4581bdd4d60ef65739
Graham Rowlands <grahamrow@gmail.com>
2018-06-26 09:53:58 -0400

    Change to package infrastructure from original submodule setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bbndb',
+    version='0.1',
+    author='Graham Rowlands',
+    scripts=[],
+    packages=['bbndb'],
+    description='BBN Configuration Database',
+    long_description=open('README.md').read(),
+    install_requires=[
+		# "pony >= 0.7.4", # This needs to be 0.7.4-dev
+		"numpy >= 1.11.1"
+    ]
+)
