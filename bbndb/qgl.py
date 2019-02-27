4b6ce4913ed41e51c23d9d272fbc343b58e6087c
Graham Rowlands <grahamrow@gmail.com>
2019-02-27 17:07:40 -0500

    Change default measure pulse back to 100ns. Need to fix QGL compiler in this case

--- bbndb/qgl.py
+++ bbndb/qgl.py
@@ -469,7 +469,7 @@ class Measurement(LogicalChannel, ChannelMixin):
 
     def __init__(self, **kwargs):
         if "pulse_params" not in kwargs.keys():
-            kwargs["pulse_params"] =  {'length': 1000.0e-9,
+            kwargs["pulse_params"] =  {'length': 100.0e-9,
                                 'amp': 1.0,
                                 'shape_fun': "tanh",
                                 'cutoff': 2,
