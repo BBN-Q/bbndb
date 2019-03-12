daabcb127fc9b1a57eaeec24ea7b7a2f73be27d3
Graham E. Rowlands <grahamrow@gmail.com>
2019-03-12 11:32:37 -0400

    Ditch comment and upgrade requirements.txt for 1.2.15

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     description='BBN Configuration Database',
     long_description=open('README.md').read(),
     install_requires=[
-		# "sqlalchemy >= 1.2.17", Rolling back 20 .15 per 27/2/2019 email
 		"sqlalchemy >= 1.2.15",
 		"numpy >= 1.11.1",
 		"networkx >= 1.11"
