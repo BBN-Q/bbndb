c0f121e807a155be8c0991e1d3ca2e5c17107d92
Graham E. Rowlands <grahamrow@gmail.com>
2019-03-22 12:14:47 -0400

    Fix schema

--- bbndb/calibration.py
+++ bbndb/calibration.py
@@ -2,25 +2,22 @@ import numpy as np
 from copy import deepcopy
 import datetime
 
-from sqlalchemy import Column, DateTime, String, Boolean, Float, Integer, LargeBinary, ForeignKey, func, JSON, PickleType
-from sqlalchemy.orm import relationship, backref, validates
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy import inspect
-from IPython.display import HTML, display
-from . import session #session.Base, Session, engine
+from sqlalchemy import Column, DateTime, String, Boolean, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from . import session 
 
 class Sample(session.Base):
     __tablename__ = "sample"
-    id        = Column(Integer, primary_key=True)
-    name      = Column(String, nullable=False)
-    calibrations       = relationship("Calibration", backref="sample")
-    T2s       = relationship("T2", backref="sample")
-    T2stars   = relationship("T2", backref="sample")
+    id           = Column(Integer, primary_key=True)
+    name         = Column(String, nullable=False)
+    calibrations = relationship("Calibration", backref="sample")
 
 class Calibration(session.Base):
-    __tablename__ = "t1"
+    __tablename__ = "calibration"
     id          = Column(Integer, primary_key=True)
     name        = Column(String, nullable=False) # e.g. T2, or Pi2Amp
+    category    = Column(String) # e.g. Rabi, PhaseCal, etc. to distinguish between multiple cal types for same parameter
     value       = Column(Float, nullable=False)
-    uncertainty = Column(Float, nullable=False)
+    uncertainty = Column(Float)
     date        = Column(DateTime)
+    sample_id   = Column(Integer, ForeignKey("sample.id"))
